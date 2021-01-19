69aaea4c69c3041684e4b57708f7abe2cef05ca1
Amber <AmberRosannee@outlook.com>
2021-01-19 16:25:06 +0100

    css van quiz aangepast

---
+++ JSB2L5_Quiz/quiz1.js
@@ -0,0 +1,117 @@
+const quiz1 =
+{
+    "quizID": "Museum 1",
+    "quizMetaData":
+    {
+        "title": "Lisser Art Museum",
+        "imageURI": "https://www.lamlisse.nl/wp-content/uploads/2018/03/LAM-Lisse-logo-white.svg"
+
+    },
+    "quizContent":		
+		[
+			{
+				"question": "Wat voor museum is het LAM?",
+				"answers": [{
+						"answer": "geschiedenis",
+						"feedback": false
+					},
+					{
+						"answer": "foodart",
+						"feedback": true
+					},
+					{
+						"answer": "natuur",
+						"feedback": false
+					}
+				]
+			},
+			{
+				"question": "Welk kunstwerk bevind zich in het LAM museum?",
+				"answers": [{
+						"answer": "de nachtwacht",
+						"feedback": false
+					},
+					{
+						"answer": "de bad grapes",
+						"feedback": true
+					},
+					{
+						"answer": "de slag bij waterloo",
+						"feedback": false
+					}
+				]
+			},
+			{
+				"question": "Waar ligt het LAM museum?",
+				"answers": [{
+						"answer": "Lisse",
+						"feedback": true
+					},
+					{
+						"answer": "Sassenheim",
+						"feedback": false
+					},
+					{
+						"answer": "Amersfoort",
+						"feedback": false
+					}
+				]
+			},
+			{
+				"question": "Welk kasteel ligt in de buurt van het museum?",
+				"answers": [{
+						"answer": "kasteel radboud",
+						"feedback": false
+					},
+					{
+						"answer": "kasteel keukenhof",
+						"feedback": true
+					},
+					{
+						"answer": "kasteel brederode",
+						"feedback": false
+					}
+				]
+			},
+			{
+				"question": "Voor welke leeftijds groep is het LAM museum?",
+				"answers": [{
+						"answer": "Kinderen en jongeren",
+						"feedback": false
+					},
+					{
+						"answer": "Volwassenen",
+						"feedback": false
+					},
+					{
+						"answer": "Voor alle leeftijden",
+						"feedback": true
+					}
+				]
+			},
+			{
+				"question": "Op welke dagen is het LAM geopend?",
+				"answers": [{
+						"answer": "Maandag t/m vrijdag",
+						"feedback": false
+					},
+					{
+						"answer": "Dinsdag t/m zaterdag",
+						"feedback": false
+					},
+					{
+						"answer": "Woensdag en vrijdag t/m zondag",
+						"feedback": true
+					}
+				]
+			}
+		]
+	}
+
+
+/*vragen voor de quiz
+6. 
+	a. 
+	b. 
+	c. W
+*/
